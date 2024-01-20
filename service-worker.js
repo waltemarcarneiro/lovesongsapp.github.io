677a38c08e113a9998949501749a9e07d3ccf11e
lovesongsplayer <157166083+lovesongsplayer@users.noreply.github.com>
2024-01-20 04:08:24 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/offline.html',
+];
+
+self.addEventListener("install", function (event) {
+  console.log("Install Event processing");
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("Cached offline page during install");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(function (response) {
+        console.log("add page to offline cache: " + response.url);
+
+        // If request was successful, add or update it in the cache
+        return caches.open(CACHE_NAME).then(function (cache) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      })
+      .catch(function (error) {
+        console.log("Network request Failed. Serving content from cache: " + error);
+        return fromCache(event.request);
+      })
+  );
+});
+
+function fromCache(request) {
+  return caches.open(CACHE_NAME).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      if (!matching || matching.status === 404) {
+        if (request.destination !== "document" || request.mode !== "navigate") {
+          return caches.match('/offline.html');
+        }
+
+        return caches.match('/offline.html');
+      }
+
+      return matching;
+    });
+  });
+}
