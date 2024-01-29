fb99261bde75485b5b1ef0d2fc02d3e26f8c229a
lovesongsplayer <157166083+lovesongsplayer@users.noreply.github.com>
2024-01-29 05:16:34 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -74,3 +74,59 @@ function fromCache(request) {
     });
   });
 }
+// Check to make sure Sync is supported.
+if ('serviceWorker' in navigator && 'SyncManager' in window) {
+
+  // Get our service worker registration.
+  const registration = await navigator.serviceWorker.registration;
+
+  try {
+    // This is where we request our sync. 
+    // We give it a "tag" to allow for differing sync behavior.
+    await registration.sync.register('database-sync');
+
+  } catch {
+    console.log("Background Sync failed.")
+  }
+}
+// Add an event listener for the `sync` event in your service worker.
+self.addEventListener('sync', event => {
+
+  // Check for correct tag on the sync event.
+  if (event.tag === 'database-sync') {
+
+    // Execute the desired behavior with waitUntil().
+    event.waitUntil(
+
+      // This is just a hypothetical function for the behavior we desire.
+      pushLocalDataToDatabase();
+    );
+    }
+});
+// Query the user for permission.
+const periodicSyncPermission = await navigator.permissions.query({
+  name: 'periodic-background-sync',
+});
+// Check if permission was properly granted.
+if (periodicSyncPermission.state == 'granted') {
+
+  // Register a new periodic sync.
+  await registration.periodicSync.register('fetch-new-content', {
+    // Set the sync to happen no more than once a day.
+    minInterval: 24 * 60 * 60 * 1000
+  });
+} 
+// Listen for the `periodicsync` event.
+self.addEventListener('periodicsync', event => {
+
+  // Check for correct tag on the periodicSyncPermissionsync event.
+  if (event.tag === 'fetch-new-content') {
+
+    // Execute the desired behavior with waitUntil().
+    event.waitUntil(
+
+      // This is just a hypothetical function for the behavior we desire.
+      fetchNewContent();
+    );
+  }
+});
